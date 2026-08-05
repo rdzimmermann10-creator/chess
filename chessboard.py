ea5148ae0d5264e7625fc5b5c86904a07f48c1c4
Ruben <rdzimmermann10@gmail.com>
2026-08-05 17:20:24 +0200

    Nur Farbe am Zug kann Figuren bewegen Wechsel der Farbe nach jedem Zug

--- chessboard.py
+++ chessboard.py
@@ -28,6 +28,13 @@ def get_square_index(x,y):
 def get_piece(index):
     return position[index]
 
+def move_piece(start, end):
+    if start not in range(64) or end not in range(64): return
+    if position[start] == 0: return
+    position[end] = position[start]
+    position[start] = 0
+
+
 def get_legal_moves_pawn(index):
     if position[index] < 0: black = True
     elif position[index] > 0: black = False
--- main.py
+++ main.py
@@ -11,7 +11,10 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 
 run = True
+turn = 1
 clicked_piece = 0
+clicked_index = -1
+legal_moves = []
 
 while run:
     mouse_pos = pygame.mouse.get_pos()
@@ -23,10 +26,23 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
         if event.type == pygame.MOUSEBUTTONDOWN:
+            # remember the piece that is currently picked
+            last_clicked_index = clicked_index
+            
             mouse_pos = pygame.mouse.get_pos()
             clicked_index = get_square_index(mouse_pos[0], mouse_pos[1])
+            if clicked_index in legal_moves:
+                move_piece(last_clicked_index, clicked_index)
+                # change turn after move
+                turn *= -1
+        
             clicked_piece = get_piece(clicked_index)
+            # check if clicked piece has the right color
+            if clicked_piece*turn < 0: 
+                clicked_piece = 0
+                clicked_index = -1
             legal_moves = get_legal_moves_pawn(clicked_index)
     
     
